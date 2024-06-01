c2faaf65727ec70219d20eb7b4d625a89f465e29
Erik Petrov <petrov_erik@outlook.com>
2024-06-01 23:10:30 +0300

    mod2 less4 sol3

---
+++ Python_fullstack_class/Module_2/lesson_4/src/solution_3.py
@@ -0,0 +1,4 @@
+# модуль 2 урок 4 задача 3
+prodact_price = float(input("Цена продукта $: "))
+dollar_rate = float(input("Курс доллара: "))
+print("Цена в местной валюте:",round(prodact_price * dollar_rate, 2))
