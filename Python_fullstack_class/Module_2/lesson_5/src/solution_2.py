86cc1ae5c410e4c65b93a97b664c8d38dba50656
Erik Petrov <petrov_erik@outlook.com>
2024-06-11 21:01:43 +0300

    mod2 less5 sol2

---
+++ Python_fullstack_class/Module_2/lesson_5/src/solution_2.py
@@ -0,0 +1,4 @@
+# модуль 2 урок 5 задача 2
+print("Welcome to the Creative Space".find("Creative"))
+print("We don't have realive in here".find("Creative"))
+print("Creative ideas for you".find("Creative"))
